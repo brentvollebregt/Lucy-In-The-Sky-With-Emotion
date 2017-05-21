fe834c4d123b58379117fdc866730283bea63559
Rhys <rhys.compton@gmail.com>
2017-05-22 09:48:09 +1200

    Main Menu
    Got a basic main menu/scene selection

---
+++ Testing/Rhys/MainMenu/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+    {
+        EditorSceneManager.LoadScene(sceneIndex);
+    }
+}
