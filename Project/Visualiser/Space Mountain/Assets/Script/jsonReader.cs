7f401dbc0f55a21fde279dde3e625d9b684bb09b
MinionSattle <sattle@TheBigMac.local>
2017-05-28 17:43:02 +1200

    json files being read and stored

--- Project/Visualiser/Space Mountain/Assets/Script/jsonReader.cs
+++ Project/Visualiser/Space Mountain/Assets/Script/jsonReader.cs
@@ -4,49 +4,52 @@ using System.IO;
 using UnityEngine;
 
 public class jsonReader : MonoBehaviour {
-
-	public class JSONFILE
+	[System.Serializable]
+	public class Song
 	{
 		public string artist,title, file_location;
 		public double bpm, songLength, valence, energy;
 	}
-	List<JSONFILE> jsonFiles;
-	string json;
+	List<Song> jsonFiles;
+	string jsonString, path;
 
 	// Use this for initialization
 	void Start () {
-		
-		jsonFiles = new	List<JSONFILE> ();
-		int songCount = Directory.GetFiles (Path.GetDirectoryName("/")).Length;
-		for (int i = 1; i <= songCount; i++) {
-			using (StreamReader r = new StreamReader ("song"+ i +".json")) {
-				string json = r.ReadToEnd ();
-				jsonFiles.Add (JsonUtility.FromJson<JSONFILE> (json));
-			}
-		}
-		Debug.Log (jsonFiles.Count);
+		path = Application.streamingAssetsPath + "/song1.json";
+		jsonString = File.ReadAllText (path);
+		Song song = JsonUtility.FromJson<Song> (jsonString);
+		Debug.Log (song.artist);
+//		jsonFiles = new	List<JSONFILE> ();
+//		int songCount = DirCount(Path.GetDirectoryName(@""));
+//		for (int i = 1; i <= songCount; i++) {
+//			using (StreamReader r = new StreamReader ("song"+ i +".json")) {
+//				string json = r.ReadToEnd ();
+//				jsonFiles.Add (JsonUtility.FromJson<JSONFILE> (json));
+//			}
+//		}
+//		Debug.Log (jsonFiles.Count);
 	}
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	void MoveJSON(){
-		string sourceFile = "";
-		string destinationFile = "";
-	}
-	void ReadJSON(){
-	}
-
-	public static long DirCount(DirectoryInfo d)
-	{
-		long i = 0;
-		FileInfo[] fis = d.GetFiles();
-		foreach (FileInfo fi in fis)
-		{
-			if (fi.Extension.Contains("json"))
-				i++;
-		}
-		return i;
-	}
+//	void MoveJSON(){
+//		string sourceFile = "";
+//		string destinationFile = "";
+//	}
+//	void ReadJSON(){
+//	}
+//
+//	public static long DirCount(DirectoryInfo d)
+//	{
+//		long i = 0;
+//		FileInfo[] fis = d.GetFiles();
+//		foreach (FileInfo fi in fis)
+//		{
+//			if (fi.Extension.Contains("json"))
+//				i++;
+//		}
+//		return i;
+//	}
 
 }
