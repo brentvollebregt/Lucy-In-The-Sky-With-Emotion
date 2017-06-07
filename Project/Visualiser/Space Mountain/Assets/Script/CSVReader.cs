a0d9d08df0af83c1702257675955bc000904e3cb
MinionSattle <sattle@TheBigMac.local>
2017-06-08 02:55:10 +1200

    changed stuff

--- Project/Visualiser/Space Mountain/Assets/Script/CSVReader.cs
+++ Project/Visualiser/Space Mountain/Assets/Script/CSVReader.cs
@@ -11,12 +11,11 @@ public class CSVReader : MonoBehaviour {
 	public Text UIDisplayText_;
 	// Use this for initialization
 	void Start () {
-		//string cvsText = File.ReadAllText (Application.streamingAssetsPath + "/visualiser_data.csv");
 		songs_ = new List<Song>();
 		StreamReader cvsText = new StreamReader(Application.streamingAssetsPath + "/visualiser_data.csv");
 		while ((songData_ = cvsText.ReadLine ()) != null) {
 			string[] words = songData_.Split(',');
-			songs_.Add (new Song (words[0],words[1], double.Parse(words[2]),double.Parse(words[3]),double.Parse(words[4]),double.Parse(words[5]),words[6]));
+			songs_.Add (new Song (words[0],words[1], double.Parse(words[2]),double.Parse(words[3]),double.Parse(words[4]),double.Parse(words[5]),words[6],words[7]));
 		}
 		SetCurrentSong ();
 	}
@@ -26,7 +25,6 @@ public class CSVReader : MonoBehaviour {
 	void Update () {
 		if (Input.GetAxis ("x") > 0) {
 			UIDisplayText_.text = currentSong_.ToString ();
-//			UIDisplayText_.text = "Reeeeeeeee";
 		} else {
 			UIDisplayText_.text = "";
 		}
@@ -35,8 +33,8 @@ public class CSVReader : MonoBehaviour {
 		}
 	}
 	public void NextSong(){
-		songs_.RemoveAt (0);
 		if (songs_.Count > 0) {
+			songs_.RemoveAt (0);
 			SetCurrentSong ();
 		} else {
 			Finished ();
@@ -53,10 +51,10 @@ public class CSVReader : MonoBehaviour {
 }
 public class Song
 {
-	string artist_,title_, file_location_;
+	string artist_,title_, fileName_, mood_;
 	double bpm_, songLength_, valence_, energy_;
 
-	public Song(string title, string artist, double bpm, double energy, double valence,double song_length,string original_file_location)
+	public Song(string title, string artist, double bpm, double energy, double valence,double song_length,string mood,string fileName)
 	{
 		title_ = title;
 		artist_ = artist;
@@ -64,12 +62,13 @@ public class Song
 		energy_ = energy;
 		valence_ = valence;
 		songLength_ = song_length;
-		file_location_= original_file_location;
+		mood_ = mood;
+		fileName_= fileName;
 		Debug.Log(this.title_);
 	}
 	public override string ToString ()
 	{
-		return string.Format ("Title: {0} \nArtist: {1}\nBPM: {2}\nEnergy: {3}\nValence: {4}\nSong Length: {5}",title_, artist_,bpm_,energy_,valence_,songLength_);
+		return string.Format ("Title: {0} \nArtist: {1}\nBPM: {2}\nEnergy: {3}\nValence: {4}\nMood: {5}",title_, artist_,bpm_,energy_,valence_,mood_);
 	}
 	public string title{ get { return title_; } }
 	public string artist{ get { return artist_; } }
@@ -77,5 +76,6 @@ public class Song
 	public double energy{ get { return energy_; } }
 	public double valence{ get { return valence_; } }
 	public double songLength{ get { return songLength_; } }
-	public string file_Location{ get { return file_location_; } }
+	public string mood{ get { return mood_; } }
+	public string fileName{ get { return fileName_; } }
 }
