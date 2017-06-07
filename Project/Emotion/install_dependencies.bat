0c5f8cbe78e0660dac8e7acc680431e2a3c644bb
Brent Vollebregt <volleys2468@gmail.com>
2017-06-07 18:54:22 +1200

    Quick install batch file
    If you want to do this quick, run this and then find the files for the
    other two modules and install them.

---
+++ Project/Emotion/install_dependencies.bat
@@ -0,0 +1,7 @@
+pip install requests
+pip install matplotlib
+pip install mutagen
+pip install pyqt5
+pip install scikit-learn
+pip install pandas
+start "" cmd /c "echo Please install scipy and numpy+mlk manually&echo(&pause"
